c6bfb399fd541c3ad6d14a8b3a87a2b0fe402354
ADMIN\PhamLinh <phamvanlinh20111993@gmail.com>
2021-05-06 23:18:51 -0700

    create safety click

--- safety-click-extension/content.js
+++ safety-click-extension/content.js
@@ -1,17 +1,24 @@
-// This constant is safe, it's just a string in base 64 that we will use below.
-const messageToShow =
-  "IyMjIyMjICAjIyMjIyMjIyAjIyMjIyMjIyAgIyMjIyMjIyMgICAgIyMjICAgICMjICAgICAjIyAjIyMjIyMjIyAjIyMjIyMjIyAgICAgIAojIyAgICAjIyAgICAjIyAgICAjIyAgICAgIyMgIyMgICAgICAgICAjIyAjIyAgICMjICAgICAjIyAjIyAgICAgICAjIyAgICAgIyMgICAgIAojIyAgICAgICAgICAjIyAgICAjIyAgICAgIyMgIyMgICAgICAgICMjICAgIyMgICMjICAgICAjIyAjIyAgICAgICAjIyAgICAgIyMgICAgIAogIyMjIyMjICAgICAjIyAgICAjIyMjIyMjIyAgIyMjIyMjICAgIyMgICAgICMjICMjICAgICAjIyAjIyMjIyMgICAjIyMjIyMjIyAgICAgIAogICAgICAjIyAgICAjIyAgICAjIyAgICMjICAgIyMgICAgICAgIyMjIyMjIyMjICAjIyAgICMjICAjIyAgICAgICAjIyAgICMjICAgICAgIAojIyAgICAjIyAgICAjIyAgICAjIyAgICAjIyAgIyMgICAgICAgIyMgICAgICMjICAgIyMgIyMgICAjIyAgICAgICAjIyAgICAjIyAgIyMjIAogIyMjIyMjICAgICAjIyAgICAjIyAgICAgIyMgIyMjIyMjIyMgIyMgICAgICMjICAgICMjIyAgICAjIyMjIyMjIyAjIyAgICAgIyMgIyMj";
+//(async () => {
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // Once we receive a message from the popup
-  if (request.msg) {
-    // If message has the `action` key `print_in_console`
-    if (request.msg.action === "print_in_console") {
-      // print awesome text on console
-      console.log(`%c ${atob(messageToShow)}`, "color:#38B549;");
-    } else if (request.msg.action === "change_body_color") {
-      // message contains different `action` key. This time it's a `change_body_color`.
-      document.body.style.background = request.msg.value;
+$(document).off("cut copy paste", "**");
+$(document).mouseover(function (e) {
+  if (e && e.target.tagName === "A") {
+    //   e.target.disabled = true;
+    console.log(e.target.href)
+    window.open({ url: encodeURI(e.target.href) });
+    e.target.removeAttribute("href");
+  } else {
+    let currentElement = e.target
+    while (currentElement && currentElement.parentElement) {
+      let parentElement = currentElement.parentElement;
+      if (parentElement.tagName === "A") {
+        console.log(parentElement.href)
+        window.open({ url: encodeURI(parentElement.href) });
+        parentElement.removeAttribute("href");
+        break;
+      }
+      currentElement = parentElement
     }
   }
-});
+})
+//})
